7e300a3a091fc8d09cd1aba8e4781918b7555ca3
pedromeireles23 <pemeirels@gmail.com>
2026-05-11 01:06:07 -0300

    Retirando responsibilidades de Player e Enemy

--- TurnBasedRpg/BattleSystem/Battle.cs
+++ TurnBasedRpg/BattleSystem/Battle.cs
@@ -3,6 +3,8 @@ public class Battle
     public Player Player { get; private set; }
     public Enemy Enemy { get; private set; }
 
+    public int CountRounds { get; private set; } = 1;
+
     public Battle(Player player, Enemy enemy)
     {
         Player = player;
@@ -10,9 +12,80 @@ public class Battle
     }
 
     //TODO fazer lógica da batalha
-    public void StartCombat() { }
 
-    public void RunRound() { }
+    public void StartCombat()
+    {
+        while (CheckDeath() == false)
+        {
+            RunRound();
+            CountRounds++;
+        }
+    }
 
-    public void CheckDeath() { }
+    public void RunRound()
+    {
+        Console.WriteLine($"Round: {CountRounds}");
+        if (Player.SpeedPoints >= Enemy.SpeedPoints)
+        {
+            // Enemy.ReceiveDamage(Player.Attack());
+            Console.WriteLine($"Player: {Player.Name} attacked {Enemy.Name} ");
+            Console.WriteLine($"Damage caused: {Enemy.ReceiveDamage(Player.Attack())}");
+            Console.WriteLine($"Enemy HP:{Enemy.CurrentHealth}");
+
+            if (CheckDeath() == true)
+            {
+                Console.WriteLine("Enemy is Dead");
+            }
+            else
+            {
+                Console.WriteLine($"Enemy: {Enemy.Name} attacked {Player.Name} ");
+                Console.WriteLine($"Damage caused: {Player.ReceiveDamage(Enemy.Attack())}");
+                Console.WriteLine($"Player HP:{Player.CurrentHealth}");
+                if (CheckDeath() == true)
+                {
+                    Console.WriteLine("Player is Dead");
+                }
+                Console.WriteLine("Rodada Finalizada");
+            }
+        }
+        else
+        {
+            // Player.ReceiveDamage(Enemy.Attack());
+            Console.WriteLine($"Enemy: {Enemy.Name} attacked {Player.Name} ");
+            Console.WriteLine($"Damage caused: {Player.ReceiveDamage(Enemy.Attack())}");
+            Console.WriteLine($"Player HP:{Player.CurrentHealth}");
+            if (CheckDeath() == true)
+            {
+                Console.WriteLine("Player is Dead");
+            }
+            else
+            {
+                Console.WriteLine($"Player: {Player.Name} attacked {Enemy.Name} ");
+                Console.WriteLine($"Damage caused: {Enemy.ReceiveDamage(Player.Attack())}");
+                Console.WriteLine($"Enemy HP:{Enemy.CurrentHealth}");
+
+                if (CheckDeath() == true)
+                {
+                    Console.WriteLine("Enemy is Dead");
+                }
+                Console.WriteLine("Rodada Finalizada");
+            }
+        }
+    }
+
+    public bool CheckDeath()
+    {
+        if (Player.CurrentHealth <= 0)
+        {
+            return true;
+        }
+        else if (Enemy.CurrentHealth <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
---
+++ TurnBasedRpg/Entities/Character.cs
@@ -0,0 +1,69 @@
+public class Character
+{
+    public string Name { get; set; }
+    public int HpPoints { get; private set; }
+    public int ManaPoints { get; private set; }
+    public int AttackPoints { get; private set; }
+    public int DefensePoints { get; private set; }
+
+    public int CurrentHealth { get; private set; }
+    public int SpeedPoints { get; private set; }
+    public CharacterClass CharacterClass { get; private set; }
+
+    public Character(string name, CharacterClass characterClass)
+    {
+        Name = name;
+        CharacterClass = characterClass;
+        switch (characterClass)
+        {
+            case CharacterClass.Mago:
+                HpPoints = 70;
+                CurrentHealth = 70;
+                AttackPoints = 12;
+                ManaPoints = 120;
+                DefensePoints = 6;
+                SpeedPoints = 8;
+                break;
+            case CharacterClass.Guerreiro:
+                HpPoints = 140;
+                CurrentHealth = 140;
+                AttackPoints = 20;
+                ManaPoints = 20;
+                DefensePoints = 18;
+                SpeedPoints = 6;
+                break;
+            case CharacterClass.Arqueiro:
+                HpPoints = 100;
+                CurrentHealth = 100;
+                AttackPoints = 16;
+                ManaPoints = 40;
+                DefensePoints = 10;
+                SpeedPoints = 12;
+                break;
+            case CharacterClass.Ladino:
+                HpPoints = 90;
+                CurrentHealth = 90;
+                AttackPoints = 18;
+                ManaPoints = 30;
+                DefensePoints = 8;
+                SpeedPoints = 18;
+                break;
+            default:
+                throw new ArgumentException("Classe inválida");
+        }
+    }
+
+    public int Attack()
+    {
+        return AttackPoints;
+    }
+
+    public int ReceiveDamage(int attackPoints)
+    {
+        var dano = Math.Max(1, attackPoints - DefensePoints);
+        CurrentHealth -= dano;
+        if (CurrentHealth <= 0)
+            CurrentHealth = 0;
+        return dano;
+    }
+}
